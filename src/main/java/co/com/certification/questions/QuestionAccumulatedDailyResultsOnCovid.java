48af873a112370849100021fd7ec62c1130ae4ea
Luis Restrepo <luferest@bancolombia.com.co>
2026-05-28 12:27:25 -0500

    Ajustar code smells

--- src/main/java/co/com/certification/questions/QuestionAccumulatedDailyResultsOnCovid.java
+++ src/main/java/co/com/certification/questions/QuestionAccumulatedDailyResultsOnCovid.java
@@ -5,7 +5,6 @@ import co.com.certification.utils.others.Utils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.core.Serenity;
-import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 import org.jetbrains.annotations.Contract;
